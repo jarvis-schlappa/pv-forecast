db5f172faf7b0243cac7ef3642da43d8f40c35f5
jarvis-schlappa <jarvis@schlappa.de>
2026-02-06 13:56:38 +0100

    fix: urllib3 LibreSSL-Warnung unterdrücken auf macOS

--- src/pvforecast/__init__.py
+++ src/pvforecast/__init__.py
@@ -4,4 +4,9 @@ PV-Forecast: Ertragsprognose für Photovoltaik-Anlagen.
 Verwendet historische PV-Daten und Wettervorhersagen für 48h-Prognosen.
 """
 
+# urllib3 v2 Warnung auf macOS unterdrücken (LibreSSL statt OpenSSL)
+import warnings
+
+warnings.filterwarnings("ignore", message="urllib3 v2 only supports OpenSSL")
+
 __version__ = "0.1.0"
